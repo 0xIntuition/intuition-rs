9565b22b80cd8acfed4a960517a1cda16d22dbfc
Simonas Karuzas <simonas@not.cat>
2024-11-12 21:08:40 +0200

    fix: signals acount label

--- cli/src/ui/signals.rs
+++ cli/src/ui/signals.rs
@@ -26,14 +26,11 @@ pub fn draw(f: &mut Frame, app: &App, area: Rect) {
                 .unwrap_or("N/A")
                 .to_string();
 
-            // let account_label = signal
-            //     .account
-            //     .as_ref() // Add this line to get a reference first
-            //     .and_then(|account| account.label.as_deref())
-            //     .unwrap_or("N/A");
-
-            // FIXME: This is a temporary fix to get the code to compile
-            let account_label = "N/A";
+            let account_label = signal
+                .account
+                .as_ref()
+                .and_then(|account| Some(account.label.clone()))
+                .unwrap_or("N/A".to_string());
 
             Row::new(vec![
                 Cell::from(account_label),
