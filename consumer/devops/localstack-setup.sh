6e86c97108fcd4b9b4f9c15fbf1580548a6dfeba
leboiko <luiseduardo.boiko@gmail.com>
2024-12-04 17:05:42 -0300

    Fixing visibility timeout

--- consumer/devops/localstack-setup.sh
+++ consumer/devops/localstack-setup.sh
@@ -3,4 +3,4 @@ echo "Initializing localstack SQS..."
 awslocal sqs create-queue --queue-name raw_logs
 awslocal sqs create-queue --queue-name decoded_logs
 awslocal sqs create-queue --queue-name resolver
-awslocal sqs create-queue --queue-name ipfs_upload --visibility-timeout 600
+awslocal sqs create-queue --queue-name ipfs_upload --attributes VisibilityTimeout=600
