55c8cb79abb076ae014c18953e302bbaea837dce
Simonas Karuzas <simonas@not.cat>
2024-12-06 17:38:07 +0200

    Update cli.sh
    Co-authored-by: Luis Eduardo Boiko Ferreira <luiseduardo.boiko@gmail.com>
    Signed-off-by: Simonas Karuzas <simonas@not.cat>

--- cli.sh
+++ cli.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker run -i -t --env INTUITION_URL=http://host.docker.internal:8080/v1/graphql  ghcr.io/0xintuition/cli:latest cli
+#!/bin/bash
+docker run -i -t \
+  --network intuition-rs_default \
+  --env INTUITION_URL=http://graphql-engine:8080/v1/graphql \
+  ghcr.io/0xintuition/cli:latest cli
+
