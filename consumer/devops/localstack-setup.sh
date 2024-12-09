ee580831616743687c72bd7e76597f3b256e35de
leboiko <luiseduardo.boiko@gmail.com>
2024-12-09 16:04:27 -0300

    Fixing s3

--- consumer/devops/localstack-setup.sh
+++ consumer/devops/localstack-setup.sh
@@ -4,4 +4,4 @@ awslocal sqs create-queue --queue-name raw_logs
 awslocal sqs create-queue --queue-name decoded_logs
 awslocal sqs create-queue --queue-name resolver
 awslocal sqs create-queue --queue-name ipfs_upload --attributes VisibilityTimeout=600
-awslocal s3api create-bucket --bucket consumer-logs
+awslocal s3api create-bucket --bucket consumer-logs --create-bucket-configuration LocationConstraint=us-west-2
