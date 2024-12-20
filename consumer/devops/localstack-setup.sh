4b04a9f8a54284b08dcbc57026f42d90919d6c9c
leboiko <luiseduardo.boiko@gmail.com>
2024-12-20 17:12:08 -0300

    Fixing DB url

--- consumer/devops/localstack-setup.sh
+++ consumer/devops/localstack-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Initializing localstack SQS..."
-awslocal sqs create-queue --queue-name raw_logs
-awslocal sqs create-queue --queue-name decoded_logs
+awslocal sqs create-queue --queue-name raw_logs.fifo --attributes FifoQueue=true,ContentBasedDeduplication=true
+awslocal sqs create-queue --queue-name decoded_logs.fifo --attributes FifoQueue=true,ContentBasedDeduplication=true
 awslocal sqs create-queue --queue-name resolver
 awslocal sqs create-queue --queue-name ipfs_upload --attributes VisibilityTimeout=600
