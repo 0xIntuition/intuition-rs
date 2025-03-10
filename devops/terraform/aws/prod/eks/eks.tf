6b9efe3b707e209ad6c7d60c3aff3163335e2c1e
francoispqt <francois.parquet@wave.com>
2025-03-10 21:50:06 +0100

    add nodepool to prod-eks

--- devops/terraform/aws/prod/eks/eks.tf
+++ devops/terraform/aws/prod/eks/eks.tf
@@ -25,10 +25,10 @@ module "eks" {
       instance_types = ["t3.medium"]
 
       min_size = 1
-      max_size = 5
+      max_size = 6
       # This value is ignored after the initial creation
       # https://github.com/bryantbiggs/eks-desired-size-hack
-      desired_size = 4
+      desired_size = 6
 
       iam_role_additional_policies = {
         AmazonEKS_CNI_Policy = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
@@ -36,6 +36,34 @@ module "eks" {
         AmazonRDSFullAccess = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
         AmazonSQSFullAccess = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
         AmazonEC2FullAccess = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+      }
+    }
+    m5xlarge_nodes = {
+      instance_types = ["m5.xlarge"]
+
+      min_size = 1
+      max_size = 2
+      # This value is ignored after the initial creation
+      # https://github.com/bryantbiggs/eks-desired-size-hack
+      desired_size = 2
+
+      capacity = "SPOT"
+
+      labels = {
+        "spot" = "true"
+      }
+
+      iam_role_additional_policies = {
+        AmazonEKS_CNI_Policy = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+        SecretsManagerReadWrite = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
+        AmazonRDSFullAccess = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+        AmazonSQSFullAccess = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+        AmazonEC2FullAccess = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+      }
+
+      launch_template = {
+        id      = aws_launch_template.custom_lt_xlarge_maxnodes.id
+        version = "$Latest"
       }
     }
   }
