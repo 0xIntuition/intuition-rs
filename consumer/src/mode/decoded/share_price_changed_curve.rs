0ae76faa482f6d31e59004236468fd2155647163
leboiko <luiseduardo.boiko@gmail.com>
2025-03-21 01:10:57 -0300

    Improving the share price changed curve handler

--- consumer/src/mode/decoded/share_price_changed_curve.rs
+++ consumer/src/mode/decoded/share_price_changed_curve.rs
@@ -10,7 +10,7 @@ use models::{
     },
     traits::SimpleCrud,
     types::U256Wrapper,
-    vault::{CurveVaultTerms, Vault},
+    vault::Vault,
 };
 use tracing::info;
 
@@ -23,32 +23,22 @@ impl SharePriceChangedCurve {
     ) -> Result<(), ConsumerError> {
         info!("Processing SharePriceChangedCurve event: {:?}", self);
 
-        // Get the curve number from the event
-        let curve_number = U256Wrapper::from(self.curveId);
-
         // Get the vault ID from the event
         let vault_id = U256Wrapper::from(self.termId);
 
-        // Find the base vault to determine if it's for an atom or triple
-        let base_vault = Vault::find_by_id(
+        // Find the curve_vault
+        let curve_vault = Vault::find_by_id(
             vault_id,
-            &decoded_consumer_context.pg_pool,
-            &decoded_consumer_context.backend_schema,
-        )
-        .await?
-        .ok_or(ConsumerError::VaultNotFound)?;
-
-        // Find the curve vault using the composite key
-        let curve_vault = Vault::find_by_term(
-            CurveVaultTerms::new(&base_vault, curve_number.clone()),
             &decoded_consumer_context.pg_pool,
             &decoded_consumer_context.backend_schema,
         )
         .await?;
 
-        // If the curve vault exists, update its share price
+        // If the curve vault exists, update its share price and curve_id.
+        // With this, the vault became a curve vault.
         if let Some(mut curve_vault) = curve_vault {
             curve_vault.current_share_price = U256Wrapper::from(self.newSharePrice);
+            curve_vault.curve_id = Some(U256Wrapper::from(self.curveId));
 
             curve_vault
                 .upsert(
