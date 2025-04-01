7078a556fa68c4380117d107c42452df4c1df9ba
leboiko <luiseduardo.boiko@gmail.com>
2025-03-31 22:43:42 -0300

    Adding missing fields on migrations

--- hasura/migrations/intuition/1742670130121_add_share_price_changed/up.sql
+++ hasura/migrations/intuition/1742670130121_add_share_price_changed/up.sql
@@ -4,6 +4,9 @@ CREATE TABLE share_price_changed (
     share_price NUMERIC(78, 0) NOT NULL,
     total_assets NUMERIC(78, 0) NOT NULL,
     total_shares NUMERIC(78, 0) NOT NULL,
+    block_number NUMERIC(78, 0) NOT NULL,
+    block_timestamp BIGINT NOT NULL,
+    transaction_hash TEXT NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -14,6 +17,9 @@ CREATE TABLE share_price_changed_curve (
     share_price NUMERIC(78, 0) NOT NULL,
     total_assets NUMERIC(78, 0) NOT NULL,
     total_shares NUMERIC(78, 0) NOT NULL,
+    block_number NUMERIC(78, 0) NOT NULL,
+    block_timestamp BIGINT NOT NULL,
+    transaction_hash TEXT NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -23,3 +29,11 @@ CREATE INDEX idx_share_price_changed_updated_at ON share_price_changed(updated_a
 CREATE INDEX idx_share_price_changed_curve_updated_at ON share_price_changed_curve(updated_at);
 CREATE INDEX idx_share_price_changed_term_updated_at ON share_price_changed(updated_at);
 CREATE INDEX idx_share_price_changed_curve_term_updated_at ON share_price_changed_curve(updated_at);
+CREATE INDEX idx_share_price_changed_block_number ON share_price_changed(block_number);
+CREATE INDEX idx_share_price_changed_curve_block_number ON share_price_changed_curve(block_number);
+CREATE INDEX idx_share_price_changed_term_block_number ON share_price_changed(block_number);
+CREATE INDEX idx_share_price_changed_curve_term_block_number ON share_price_changed_curve(block_number);
+CREATE INDEX idx_share_price_changed_transaction_hash ON share_price_changed(transaction_hash);
+CREATE INDEX idx_share_price_changed_curve_transaction_hash ON share_price_changed_curve(transaction_hash);
+CREATE INDEX idx_share_price_changed_term_transaction_hash ON share_price_changed(transaction_hash);
+CREATE INDEX idx_share_price_changed_curve_term_transaction_hash ON share_price_changed_curve(transaction_hash);
