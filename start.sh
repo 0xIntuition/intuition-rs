6adabde89ca32cd15823a4c86cce78365ad83fbf
Simonas Karuzas <simonas@not.cat>
2025-04-17 10:46:42 +0300

    chore: lint

--- start.sh
+++ start.sh
@@ -4,8 +4,6 @@ source .env
 # Start shared services
 docker compose -f docker-compose-shared.yml up -d --wait --force-recreate
 
-
-
 # Select contract_address from histocrawler.app_config
 CONTRACT_ADDRESS=$(docker compose -f docker-compose-shared.yml exec database psql -U testuser -d storage -c "SELECT contract_address FROM histocrawler.app_config WHERE indexer_schema = 'histo_base_sepolia_1_5'" -tA)
 echo "Contract address: $CONTRACT_ADDRESS"
