e8b34f373c658fdd8de88e6207d9bc4ffaac23d4
Simonas Karuzas <simonas@not.cat>
2025-08-04 13:48:37 +0300

    feat: contract deployer (wip)

--- start.sh
+++ start.sh
@@ -14,6 +14,38 @@ if [ -n "$CONTRACT_ADDRESS" ]; then
     export INDEXER_SCHEMA=$INDEXER_SCHEMA
 fi
 
+# If started with arg histo_local_2_0 deploy contract to local geth and get contract address
+if [ "$INDEXER_SCHEMA" == "histo_local_2_0" ]; then
+    docker compose -f docker-compose-shared.yml up contract-deployer-2-0 geth -d --wait 
+
+    docker compose -f blockscout/docker-compose.yml up -d --wait
+
+    docker compose -f docker-compose-shared.yml up contract-verifier-2-0 -d 
+    
+    # Select contract_address from histocrawler.app_config wait until it changes from 0x63B90A9c109fF8f137916026876171ffeEdEe714 or empty
+    while [ "$CONTRACT_ADDRESS" == "0x63B90A9c109fF8f137916026876171ffeEdEe714" ] || [ -z "$CONTRACT_ADDRESS" ]; do
+        CONTRACT_ADDRESS=$(docker compose -f docker-compose-shared.yml exec database psql -U postgres -d storage -c "SELECT contract_address FROM histocrawler.app_config WHERE indexer_schema = 'histo_base_sepolia_2_0'" -tA)
+        sleep 1
+    done
+    
+    echo -e "\nTo run integration tests in a different terminal, run:"
+    echo -e "\n\nexport VITE_INTUITION_CONTRACT_ADDRESS=$CONTRACT_ADDRESS"
+    echo "cd integration-tests"
+    echo "pnpm test src/create-person.test.ts"
+
+    echo -e "\nExplore the contract on blockscout:"
+    echo -e "http://localhost/address/$CONTRACT_ADDRESS?tab=logs\n\n"
+    
+    # Set env vars
+    export VITE_INTUITION_CONTRACT_ADDRESS=$CONTRACT_ADDRESS
+    export INTUITION_CONTRACT_ADDRESS=$CONTRACT_ADDRESS
+    export INITIAL_CONTRACT_VERSION="v2_0"
+    export INDEXER_SCHEMA="histo_base_sepolia_2_0"
+    export BASE_SEPOLIA_RPC_URL="http://geth:8545"
+    export BASE_MAINNET_RPC_URL="http://geth:8545"
+fi
+
+
 # If started with arg histo_local_1_5 deploy contract to local geth and get contract address
 if [ "$INDEXER_SCHEMA" == "histo_local_1_5" ]; then
     docker compose -f docker-compose-shared.yml up contract-deployer-1-5 geth -d --wait 
