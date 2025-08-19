698d331115e0da47b7f15687ca2511f95e68200e
leboiko <luiseduardo.boiko@gmail.com>
2025-08-19 11:10:45 -0300

    Fix version on start script

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ source .env
 # Start shared services
 docker compose -f docker-compose-shared.yml up database pgai-installer vectorizer-worker redis redis-setup ipfs safe-content graphql-engine local-migrations indexer-migrations hasura-migrations prometheus -d --wait --force-recreate
 
-export INITIAL_CONTRACT_VERSION="v1"
+export INITIAL_CONTRACT_VERSION="v2"
 # First arg is indexer schema
 INDEXER_SCHEMA="$1"
 CONTRACT_ADDRESS=$(docker compose -f docker-compose-shared.yml exec database psql -U postgres -d storage -c "SELECT contract_address FROM histocrawler.app_config WHERE indexer_schema = '$INDEXER_SCHEMA'" -tA)
