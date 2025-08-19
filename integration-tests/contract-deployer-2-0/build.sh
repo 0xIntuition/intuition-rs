562e8bb72b5994db3be1a999ecf0afee3819b171
Simonas Karuzas <simonas@not.cat>
2025-08-19 16:09:40 +0300

    feat: integration tests v2 (#136)
    * feat: initial poc
    
    * cargo
    
    * feat: creating multiple atoms
    
    * feat: triples
    
    * feat: fixed atom creation
    
    * feat: contracts v2 integration tests
    
    * chore: cleant workspace
    
    ---------
    
    Co-authored-by: leboiko <luiseduardo.boiko@gmail.com>

--- integration-tests/contract-deployer-2-0/build.sh
+++ integration-tests/contract-deployer-2-0/build.sh
@@ -2,10 +2,11 @@
 
 # Clone the repo locally
 rm -rf intuition-contracts-v2
-git clone --branch deploy-to-geth-3 --depth 1 git@github.com:0xIntuition/intuition-contracts-v2
+git clone --depth 1 git@github.com:0xIntuition/intuition-v2-sol-refactor
 
+cp intuition-v2-sol-refactor/.env.example intuition-v2-sol-refactor/.env
 # Build the Docker image
 docker build -f integration-tests/contract-deployer-2-0/Dockerfile.copy -t ghcr.io/0xintuition/contract-deployer-2-0:latest .
 
 # Clean up
-rm -rf intuition-contracts-v2
+rm -rf intuition-v2-sol-refactor
