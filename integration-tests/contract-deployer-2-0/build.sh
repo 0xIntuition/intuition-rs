b67529eef4ad313ed864459c9094f7b8852686c7
Simonas Karuzas <simonas@not.cat>
2025-08-27 00:32:17 +0300

    feat: deploying migration mode contract

--- integration-tests/contract-deployer-2-0/build.sh
+++ integration-tests/contract-deployer-2-0/build.sh
@@ -2,11 +2,11 @@
 
 # Clone the repo locally
 rm -rf intuition-contracts-v2
-git clone --depth 1 git@github.com:0xIntuition/intuition-v2-sol-refactor
+git clone --depth 1 --branch multivault-migration-mode-script-changes git@github.com:0xIntuition/intuition-contracts-v2.git 
 
-cp intuition-v2-sol-refactor/.env.example intuition-v2-sol-refactor/.env
+cp intuition-contracts-v2/.env.example intuition-contracts-v2/.env
 # Build the Docker image
 docker build -f integration-tests/contract-deployer-2-0/Dockerfile.copy -t ghcr.io/0xintuition/contract-deployer-2-0:latest .
 
 # Clean up
-rm -rf intuition-v2-sol-refactor
+rm -rf intuition-contracts-v2
