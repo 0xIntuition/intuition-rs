815cfbf2ffd56cf98b41313de6856322ffd29359
Luis Eduardo Boiko Ferreira <luiseduardo.boiko@gmail.com>
2025-09-05 11:33:17 -0300

    Adding migrations for intuition-testnet-d (#149)

---
+++ indexer-and-cache-migrations/1756350653_create_intuition_testnet_d_raw_data.down.sql
@@ -0,0 +1,5 @@
+-- Remove the app_config entry first
+DELETE FROM histocrawler.app_config WHERE indexer_schema = 'intuition_testnet_d';
+
+-- Drop the schema and all its contents
+DROP SCHEMA IF EXISTS intuition_testnet_d CASCADE;
