3542de2b64b7581ddcc2c83c7416cc126d0e1733
Simonas Karuzas <simonas@not.cat>
2025-09-18 18:57:14 +0300

    test: deposit on person atom

--- integration-tests/src/deposit-person.test.ts
+++ integration-tests/src/deposit-person.test.ts
@@ -1,15 +1,23 @@
 import { expect, test } from 'vitest'
-import { getIntuition } from './setup/utils.js'
+import { getIntuition, pinJson } from './setup/utils.js'
 import { parseEther } from 'viem'
 
 test('deposit on person atom', async () => {
   const alice = await getIntuition(3)
 
-  const aliceAccount = await alice.getOrCreateAtom(
-    alice.account.address
-  )
+  const uri = await pinJson({
+    '@context': 'https://schema.org',
+    '@type': 'Person',
+    name: 'Alice',
+    description: 'Intern at Intuition Systems',
+    email: 'alice@intuition.systems',
+    image: 'https://avatars.githubusercontent.com/u/94311139?s=200&v=4',
+    url: 'https://intuition.systems',
+  })
 
-  const hash = await alice.contract.write.deposit([alice.account.address, aliceAccount.vaultId, 1n, 0n], {
+  const alicePerson = await alice.getOrCreateAtom(uri)
+
+  const hash = await alice.contract.write.deposit([alice.account.address, alicePerson.vaultId, 1n, 0n], {
     value: parseEther('0.5')
   })
 
