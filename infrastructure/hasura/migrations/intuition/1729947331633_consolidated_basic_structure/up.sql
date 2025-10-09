946c28f2900e423f58c5c9ee4bddc2136a891316
leboiko <luiseduardo.boiko@gmail.com>
2025-10-09 10:32:04 -0300

    Removing duplicate index

--- infrastructure/hasura/migrations/intuition/1729947331633_consolidated_basic_structure/up.sql
+++ infrastructure/hasura/migrations/intuition/1729947331633_consolidated_basic_structure/up.sql
@@ -68,8 +68,6 @@ CREATE TABLE term (
   updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
 );
 
-CREATE INDEX idx_term_created_at ON term(created_at);
-
 CREATE TABLE atom (
   term_id TEXT PRIMARY KEY NOT NULL,
   wallet_id TEXT REFERENCES account(id) NOT NULL,
